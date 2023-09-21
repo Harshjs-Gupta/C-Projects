5a13de369962244eb55180815c9830eaaed1b7d0
Harsh Gupta <harshgupta88911@gmail.com>
2023-09-21 14:29:14 +0530

    Transpose of elements

---
+++ TransposeElement.c
@@ -0,0 +1,44 @@
+// #include<stdio.h>
+
+// int main(){
+
+//    int matrix[3][4];
+//    int trans[3][4];
+
+//    printf("Enter Matrix Element \n");
+//    for(int i=0;i<3;i++)
+//    {
+//    	for(int j=0;j<4;j++)
+//    	{
+//    		scanf("%d",&matrix[i][j]);
+//    	}
+//    }
+//    printf("Matrix Element: \n");
+//    for(int i=0;i<3;i++)
+//    {
+//    	for(int j=0;j<4;j++)
+//    	{
+//    		printf("%d ",matrix[i][j]);
+//    	}
+//    	printf("\n");
+//    }
+//    for(int i=0;i<3;i++)
+//    {
+//    	for(int j=0;j<4;j++)
+//    	{
+//    		trans[j][i]=matrix[i][j];
+//    	}
+//    }
+//    printf("Transpose Element: \n");
+//    for(int i=0;i<4;i++)
+//    {
+//    	for(int j=0;j<3;j++)
+//    	{
+//    		printf("%d",trans[i][j]);
+//    	}
+//    	printf("\n");
+//    }
+
+
+//     return 0;
+// }
