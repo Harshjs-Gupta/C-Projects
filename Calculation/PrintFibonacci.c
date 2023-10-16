b13006a790067a55d082094c8618a3d2bc4e5dc7
Harsh Gupta <harshgupta88911@gmail.com>
2023-10-16 11:53:06 +0530

    New file is craeted

---
+++ Calculation/PrintFibonacci.c
@@ -0,0 +1,16 @@
+//write a c program to display fibonacci series
+// #include<stdio.h>
+// int main()
+// { 
+//    int a=0,b=1,c,n;
+//    printf("Enter any number: ");
+//    scanf("%d",&n);
+//    for(int i=1;i<=n;i++)
+//    {
+//     c=a+b;
+//     a=b;
+//     b=c;
+//     printf("%d\n",c);
+//    }
+//     return 0;
+// }
