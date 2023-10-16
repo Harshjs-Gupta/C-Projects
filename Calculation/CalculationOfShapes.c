4fa0dda255e07775432915eb1d99c515bc7b5d53
Harsh Gupta <harshgupta88911@gmail.com>
2023-10-16 11:50:48 +0530

    New file is created

---
+++ Calculation/CalculationOfShapes.c
@@ -0,0 +1,33 @@
+// write a program to find the perimeter and area of rectangle
+
+// #include <stdio.h>
+// int main()
+// {
+//     float area, perimeter;
+//     float length, breadth;
+
+//     printf("Enter the length and breadth: \n");
+//     scanf("%f %f", &length, &breadth);
+
+//     area = length * breadth;
+//     perimeter = 2 * (length + breadth);
+
+//     printf("%f is the area of rectangle \n", area);
+//     printf("%f is the perimeter of rectangle", perimeter);
+
+//     return 0;
+// }
+
+
+//write a program to find area of cirele
+// #include<stdio.h>
+// int main()
+// {
+// float side;
+//    printf("enetr side");
+//    scanf("%f",&side);
+   
+//    printf("area is: %f",side*side);
+// return 0;
+// }
+
