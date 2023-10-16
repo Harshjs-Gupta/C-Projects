d82c10dd0046bd9e7c61fbcd09bae28cdbc8fefd
Harsh Gupta <harshgupta88911@gmail.com>
2023-10-16 11:52:01 +0530

    New file is created

---
+++ Calculation/MethamaticsCalculation.c
@@ -0,0 +1,90 @@
+//write a program to find the factorial 
+// #include<stdio.h>
+// int main()
+// {
+
+// int n,fact=1;
+//  printf("enter number : ");
+//  scanf("%d",&n);
+//  for(int i=1;i<=n;i++)
+//  {
+//    fact=fact*i;
+//  }
+//  printf("final factorial is %d ",fact);
+
+
+// return 0;
+// }
+
+
+//write a program to find the GST
+// #include<stdio.h>
+// int main()
+// {
+
+//   float price[3];
+
+// printf("enter 3 price : ");
+// scanf("%f",&price[0]);
+// scanf("%f",&price[1]);
+// scanf("%f",&price[2]);
+
+// printf("total price 1: %f\n",price[0]+(0.18*price[0]));
+// printf("total price 2: %f\n",price[1]+(0.18*price[1]));
+// printf("total price 3: %f\n",price[2]+(0.18*price[2]));
+
+//   return 0;
+// }
+
+
+//write a program to print Multiplication table 
+// #include<stdio.h>
+// int main()
+// {
+
+// int n;
+//  printf("enter number");
+//  scanf("%d",&n);
+//  for(int i=1;i<=10;i++)
+//  {
+//    printf("%d*%d=%d \n",n,i,i*n);
+//  }
+ 
+// return 0;
+// }
+
+
+
+//write a c program to calculate the sum of natural number
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     int i=1,sum=0;
+//     while(i<=n)
+//     {
+//         sum+=i;
+//         i++;
+//     }
+//      printf("sum is %d",sum);
+//     return 0;
+// }
+
+
+//write a program to check if a number is divisible by 2 or not
+// #include<stdio.h>
+// int main()
+// {
+
+// int n;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//       if(n%2==0)
+//       {
+//       printf("%d is divisible by 2");
+//       }
+
+// return 0;
+// }
