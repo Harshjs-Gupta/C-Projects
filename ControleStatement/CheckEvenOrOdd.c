fee76a3033a5fc6a60176cef14e6f9d59f9e4fd8
Harsh Gupta <harshgupta88911@gmail.com>
2023-10-26 10:29:32 +0530

    Check The number is even or odd

---
+++ ControleStatement/CheckEvenOrOdd.c
@@ -0,0 +1,17 @@
+//write a c program to check the number is even or odd
+// #include<stdio.h>
+// int main()
+// {
+//   int n;
+//   printf("Enter the number: ");
+//   scanf("%d",&n);
+//   if(n%2==0)
+//   {
+//     printf("%d is even number",n);
+//   }
+//   else
+//   {
+//     printf("%d is odd number",n);
+//   }
+//     return 0;
+// }
