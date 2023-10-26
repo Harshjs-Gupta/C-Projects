771935bbedc8fb97b87fa83d09d2ace82c751f34
Harsh Gupta <harshgupta88911@gmail.com>
2023-10-26 10:31:23 +0530

    Check The yeear is leap year or not

---
+++ ControleStatement/CheckLecpYear.c
@@ -0,0 +1,17 @@
+//write a c program to check the year is leap year or not
+// #include<stdio.h>
+// int main()
+// {
+//    int year;
+//    printf("Enter the year: ");
+//    scanf("%d",&year);
+//    if(year%400==0 || year%100 !=0 && year%4==0)
+//    {
+//     printf("%d is leap year",year);
+//    }
+//    else
+//    {
+//     printf("%d is not a leap year",year);
+//    }
+//       return 0;
+//    }
