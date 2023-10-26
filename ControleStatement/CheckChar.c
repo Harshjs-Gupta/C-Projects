6beb110f7a76b95bad50a9b28ab866ad75afde4a
Harsh Gupta <harshgupta88911@gmail.com>
2023-10-26 10:26:09 +0530

    Check Character

---
+++ ControleStatement/CheckChar.c
@@ -0,0 +1,27 @@
+// write a c program to check uppercase,lowercase,special character or digits
+// #include<stdio.h>
+// int main()
+// {
+//    char ch;
+//    printf("Enter any thing: ");
+//    scanf("%c",&ch);
+//    if(ch>='A' && ch<='Z')
+//    {
+//     printf("%c is uppercase",ch);
+//    }
+//    else if(ch>='a' && ch<='z')
+//    {
+//     printf("%c is lower case",ch);
+//    }
+//    else if(ch>=33 && ch<=47 || ch>=58 && ch<=64 || ch>=91 && ch<=96 
+//         || ch>=123 && ch<=126)
+//    {
+//     printf("%c is special character",ch);
+//    }
+//    else if(ch>=0)
+//    {
+//     printf("%d is digits",ch);
+//    }
+
+//     return 0;
+// }
