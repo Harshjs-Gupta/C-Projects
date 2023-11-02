5cedd2f84c52c4bc82c48d22ae8a39b8557cb35c
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-02 11:08:27 +0530

    New file is created

---
+++ Loop/CheckArmstrong.c
@@ -0,0 +1,26 @@
+//write a c program to find number is armstrong or not 
+// #include<stdio.h>
+// int main()
+// {
+//    int n,r;
+//    int sum=0,m;
+//    printf("Enter the number: ");
+//    scanf("%d",&n);
+//    m=n;
+//    while(n>0)
+//    {
+//     r=n%10;
+//     sum=sum+(r*r*r);
+//     n=n/10;
+//    }
+//    if(m==sum)
+//    {
+//     printf("it is a armstrong number");
+//    }
+//    else
+//    {
+//     printf("it not a armstrong number");
+//    }
+//     return 0;
+// }
+
