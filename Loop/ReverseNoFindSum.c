fba87c015e87a84a66794974cd2974d686e8219f
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-08 12:43:04 +0530

    New file is created

---
+++ Loop/ReverseNoFindSum.c
@@ -0,0 +1,17 @@
+//write a c program to reverse a number and find sum of the digits.
+// #include<stdio.h>
+// int main()
+// {
+//     int n,r,sum=0;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++)
+//     {
+//         r=n%10;
+//         n=n/10;
+//         printf("%d",r);
+//         sum=sum+r;
+//     }
+//     printf("sum is: %d",sum);
+//     return 0;
+// }
