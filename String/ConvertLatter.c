a2436e00811de52daa8520be8e8cc0ff1c91b639
Harsh Gupta <harshgupta88911@gmail.com>
2023-11-09 13:31:57 +0530

    New file is created

---
+++ String/ConvertLatter.c
@@ -0,0 +1,33 @@
+//write a c program to convert lowercase string to uppercase
+// #include<stdio.h>
+// int main()
+// {
+//       char str[30];
+//   printf("enter lower string: ");
+//   scanf("%s",&str);
+//   for(int i=0;str[i]!='\0';i++)
+//   {
+//     str[i]=str[i]-32;
+//   }
+//   printf("string is: %s",str);
+
+//     return 0;
+// }
+
+
+//write a c program to convert  uppercase to lowercase string 
+// #include<stdio.h>
+// int main()
+// {
+//     char str[30];
+//   printf("enter uppercase string: ");
+//   scanf("%s",&str);
+//   for(int i=0;str[i]!='\0';i++)
+//   {
+//     str[i]=str[i]+32;
+//   }
+//   printf("string is: %s",str);
+
+
+//     return 0;
+// }
